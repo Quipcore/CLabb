ba1f383700c9e31a4ed273569ed43064d43f869b
Felix Lidö <felix.lido@gmail.com>
2023-11-02 15:38:30 +0100

    lab1 catchup with VPL

--- lab1/labb1.cpp
+++ lab1/labb1.cpp
@@ -1,56 +1,49 @@
-//
-// Created by felix on 2023-11-01.
-//
-
-
+//Felix Lidö feli8145
 
 #include <iostream>
 #include <vector>
 
-struct Person
-{
+struct Person {
     std::string fnamn;
     std::string enamn;
     std::string nummer;
 };
 
-
-
-//Create a macro for length of array
-#define ARRAY_LENGTH_PERSON(array) (sizeof(array)/sizeof(Person))
-
-void visa_person ()
-{
+void visa_person(std::vector<Person> people, std::string fnamn) {
     // Denna funktion tar emot ett telefonregister och ett förnamn på den person-post som skall visas.
 
     // Om det finns en eller flera person-poster med detta förnamn i registret,
     // så skall telefonnumret till dessa visas, på formen: "förnamn efternamn: telefonnummer".
-//    std::cout << fnamn << " " << enamn << ": " << nummer << std::endl;
+    bool personFound = false;
+    for(const Person& person : people){
+        if(person.fnamn == fnamn){
+            std::cout << person.fnamn << " " << person.enamn << ": " << person.nummer << std::endl;
+            personFound = true;
+        }
+    }
 
     // Om ingen person-post hittas med det sökta förnamnet, så skall följande skrivas ut.
-//    std::cout << "Hittade inget nummer!" << std::endl;
+    if(!personFound){
+        std::cout << "Hittade inget nummer!" << std::endl;
+    }
 }
 
-void skriv_hela_telefonreg ()
-{
+void skriv_hela_telefonreg(const std::vector<Person>& people) {
     // Denna funktion tar emot ett telefonregister och visar samtliga poster i
     // telefonregistret på formen: "förnamn efternamn: telefonnummer".
-
-//    std::cout << fnamn << " " << enamn << ": " << nummer << std::endl;
-
+    for(const Person& person : people){
+        std::cout << person.fnamn << " " << person.enamn << ": " << person.nummer << std::endl;
+    }
 }
 
-void ny_person (std::vector<Person> people, std::string fnamn, std::string enamn, std::string nummer)
-{
-    people.push_back({fnamn, enamn, nummer});
+void ny_person(std::vector<Person> &people, std::string fnamn, std::string enamn, std::string nummer) {
     // Denna funktion tar emot ett telefonregister och ett förnamn, ett efternamn och
     // ett telefonnummer till den nya person-post som skall skapas och läggas till i telefonboken,
     // därefter läggs person-posten till registret.
-
+    people.push_back({std::move(fnamn), std::move(enamn), std::move(nummer)});
 }
 
-void ta_bort_person ()
-{
+void ta_bort_person(std::vector<Person> &people, std::string fnamn) {
     // Denna funktion tar emot ett telefonregister och ett förnamn på den person-post
     // som skall tas bort ur registret.
     // Om det finns fler person-poster med samma förnamn, så tas alla dessa bort.
@@ -58,19 +51,19 @@ void ta_bort_person ()
     // När en person-post tas bort så skrivs namnet på person-posten ut på skärmen (förnamn efternamn),
     // men ingen användardialog skall finnas, dvs. användaren skall inte bekräfta borttag.
 
-    //std::cout << fnamn << " " << enamn << " tas nu bort." << std::endl;
 
-}
-
-void print(std::vector<Person> people){
-    for(const auto & i : people){
-        std::cout << i.fnamn << " " << i.enamn << ": " << i.nummer << std::endl;
+    for(int i = 0; i < people.size(); i++){
+        Person person = people.at(i);
+        if(person.fnamn == fnamn){
+            std::cout << person.fnamn << " " << person.enamn << " tas nu bort." << std::endl;
+            people.erase(people.begin()+i);
+            --i;
+        }
     }
+
 }
 
-int main ()
-{
-
+int main() {
     // Dessa personer skall finnas med som person-poster i telefonregistret.
     /*
     "Christina", "Nyberg","0707423653"
@@ -80,18 +73,16 @@ int main ()
     "Linus", "Jonasson", "0703457923"
     "Adam", "Nordin", "0203456297"
     */
-
     // Här ska du deklarera den variabel som skall innehålla ditt register!
     std::vector<Person> people = {
-            {"Christina", "Nyberg","0707423653"},
-            {"Josefin", "Danielsson", "0705463245"},
-            {"Ellen", "Lindgren", "0705643229"},
-            {"Ellen", "Lindgren", "0705643229"},
-            {"Linus", "Jonasson", "0703457923"},
-            {"Adam", "Nordin", "0203456297"},
+            {"Christina", "Nyberg",     "0707423653"},
+            {"Josefin",   "Danielsson", "0705463245"},
+            {"Ellen",     "Lindgren",   "0705643229"},
+            {"Stig", "Ek", "0705673247"},
+            {"Linus",     "Jonasson",   "0703457923"},
+            {"Adam",      "Nordin",     "0203456297"}
     };
 
-    print(people);
 
     // Här skall en liten kommandostyrd huvudmeny skrivas. Observera att vissa kommandon
     // behöver fler värden som skall matas in. I menyn skall följande val
@@ -101,29 +92,33 @@ int main ()
     //  Skriv hela telefonboken - 's'
     //  Ta bort en person       - 'd' förnamn
     //  Avsluta                 – 'q'
+    char command = 0;
 
-
-
-    std::string command;
-
-    while(command != "q") {
+    while (command != 'q') {
         std::cout << "Kommando: ";
         std::cin >> command;
-
-        if(command == "n"){
-            std::string fnamn, enamn, nummer;
-
-            std::cout << "Name: ";
-            std::cin >> fnamn;
-
-            std::cout << "Last name: ";
-            std::cin >> enamn;
-
-            std::cout << "Number: ";
-            std::cin >> nummer;
-            ny_person(people, fnamn, enamn, nummer);
+        std::string fnamn, enamn, nummer;
+        switch (command) {
+            case 'n':
+                std::cin >> fnamn >> enamn >> nummer;
+                ny_person(people, fnamn, enamn, nummer);
+                break;
+            case 'v':
+                std::cin >> fnamn;
+                visa_person(people, fnamn);
+                break;
+            case 's':
+                skriv_hela_telefonreg(people);
+                break;
+            case 'd':
+                std::cin >> fnamn;
+                ta_bort_person(people, fnamn);
+                break;
+            default:
+                break;
         }
-
     }
+
+    std::cout << "Hejdå!" << std::endl;
     return 0;
 }
